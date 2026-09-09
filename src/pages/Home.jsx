877076df736901caf174e69d9e735331cf9b72a2
Dav <david.sanchez.martinez@outlook.com>
2026-09-09 17:25:43 +0200

    chore: move page components into pages folder

---
+++ src/pages/Home.jsx
@@ -0,0 +1,25 @@
+// import { Routes, Route, Link } from 'react-router'
+// import About from './About'
+
+const Home = () => {
+
+    return (
+        <>
+            <section className="hero-section">
+                <div className="hero-text">
+                    <h1 className='hero-heading'>
+                        You got the travel plans, we got the travel vans.
+                    </h1>
+                    <p className="hero-para">
+                        Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.
+                    </p>
+                </div>
+                <button className="hero-btn">
+                    Find your van
+                </button>
+            </section>
+        </>
+    )
+}
+
+export default Home
