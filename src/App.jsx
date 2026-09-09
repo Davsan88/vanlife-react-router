a80a7529c642c2941263d86ff431b98a4b65a8ad
Dav <david.sanchez.martinez@outlook.com>
2026-09-09 12:36:52 +0200

    chore: add navigation, routes, and footer to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,46 @@
-import { useState } from 'react'
 import './App.css'
+import Home from './components/Home'
+import About from './components/About'
+import { Routes, Route, Link } from 'react-router'
+import logo from './assets/logo.png'
 
 function App() {
-  
+
   return (
     <>
-      Hi there
+      <nav>
+        <Link to='/home'>
+          <img src={logo} alt="" />
+        </Link>
+
+        <div 
+          className="links-div"
+        >
+          <Link to='/about'>
+            About
+          </Link>
+          <Link to='/vans'>
+            Vans
+          </Link>
+        </div>
+      </nav>
+
+
+      <Routes>
+        <Route
+          path='/home'
+          element={<Home />}
+        />
+
+        <Route
+          path='/about'
+          element={<About />}
+        />
+      </Routes>
+
+      <footer>
+        © 2026 #VANLIFE
+      </footer>
     </>
   )
 }
