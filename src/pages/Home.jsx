0949e57e117c2c0e3ca6120c625a6c5cb9dff040
Dav <david.sanchez.martinez@outlook.com>
2026-09-10 12:43:48 +0200

    style: add home hero section styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
-// import { Routes, Route, Link } from 'react-router'
-// import About from './About'
+import './Home.css'
 
 const Home = () => {
 
     return (
         <>
             <section className="hero-section">
-                <div className="hero-text">
+                <div className="hero-text
+                container">
                     <h1 className='hero-heading'>
                         You got the travel plans, we got the travel vans.
                     </h1>
@@ -14,7 +14,7 @@ const Home = () => {
                         Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.
                     </p>
                 </div>
-                <button className="hero-btn">
+                <button className="hero-btn container">
                     Find your van
                 </button>
             </section>
