2b4b4cb21cbc8c4c9b8ea43358dba4e693295bc2
Dav <david.sanchez.martinez@outlook.com>
2026-09-11 10:56:49 +0200

    chore: import local Mirage JS server inside main file

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router'
 import './index.css'
 import App from './App.jsx'
+import "./server"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
