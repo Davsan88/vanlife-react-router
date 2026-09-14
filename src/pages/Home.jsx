aab1cedcc19108ab64ef3c2efc32ffbb032c038b
Dav <david.sanchez.martinez@outlook.com>
2026-09-14 17:16:40 +0200

    chore: improve home hero layout and spacing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,22 +3,20 @@ import './Home.css'
 const Home = () => {
 
     return (
-        <>
-            <section className="hero-section">
-                <div className="hero-text
+        <section className="hero-section">
+            <div className="hero-text
                 container">
-                    <h1 className='hero-heading'>
-                        You got the travel plans, we got the travel vans.
-                    </h1>
-                    <p className="hero-para">
-                        Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.
-                    </p>
-                </div>
-                <button className="hero-btn container">
-                    Find your van
-                </button>
-            </section>
-        </>
+                <h1 className='hero-heading'>
+                    You got the travel plans, we got the travel vans.
+                </h1>
+                <p className="hero-para">
+                    Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.
+                </p>
+            <button className="hero-btn container">
+                Find your van
+            </button>
+            </div>
+        </section>
     )
 }
 
