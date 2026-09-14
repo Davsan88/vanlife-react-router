3a2ccd6e9105f5d80e6761fa0250d1e9ce5f50d3
Dav <david.sanchez.martinez@outlook.com>
2026-09-14 17:12:05 +0200

    feat: implement Van detail page UI and styles

--- src/pages/VanDetail.jsx
+++ src/pages/VanDetail.jsx
@@ -1,25 +1,67 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router'
 import { useParams } from 'react-router'
 
 const VanDetail = () => {
+    const [van, setVan] = useState('')
+
     const params = useParams()
-    console.log(params)
 
     useEffect(() => {
         const loadVanDetails = async () => {
             const res = await fetch(`/api/vans/${params.id}`)
             const data = await res.json()
-            console.log(data)
+            setVan(data.vans)
         }
 
         loadVanDetails()
     }, [params.id])
 
-
     return (
-        <h1>
-            Van detail page goes here
-        </h1>
+        <>
+            <Link
+                to={'/vans/'}
+                className='van-detail-link container'
+            >
+                <i className="fa-solid fa-arrow-left-long">
+                </i>
+                <p className="back-to-vans-para">Back to all vans</p>
+            </Link>
+
+            <section className='van-detail-section container'>
+
+                <img
+                    src={van.imageUrl}
+                    alt={`Campervan called ${van.name}`}
+                    className="van-img"
+                />
+                <div className="van-details-div">
+                    <span className={`van-filter-tag ${van.type}-tag`}>
+                        {van.type}
+                    </span>
+
+                    <h1 className="van-detail-name">
+                        {van.name}
+                    </h1>
+
+                    <div className="van-price-div">
+
+                        <span className="van-price">
+                            {`$${van.price}`}
+                        </span>
+                        /day
+                    </div>
+                    <p className="van details description">
+                        {van.description}
+                    </p>
+                    <button className="hero-btn ">
+                        Rent this van
+                    </button>
+                </div>
+
+
+            </section>
+        </>
     )
 }
 
