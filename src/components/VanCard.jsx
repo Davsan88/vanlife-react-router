643e249e3fed30321fc75d1106a018317ac17e81
Dav <david.sanchez.martinez@outlook.com>
2026-09-14 13:52:17 +0200

    feat: Link van cards to a dedicated detail page using React Router

--- src/components/VanCard.jsx
+++ src/components/VanCard.jsx
@@ -1,31 +1,41 @@
+import { Link } from 'react-router'
+
 const VanCard = ({ van }) => {
 
-    const { name, price, imageUrl, type } = van
+    const { name, price, imageUrl, type, id } = van
 
     return (
-        <div className="van-div">
-            <img
-                src={imageUrl}
-                alt={`Campervan called ${name}`}
-                className="van-img"
-            />
-            <div className="van-details-div">
-                <h2 className="van-name">
-                    {name}
-                </h2>
+        <div key={id} className="van-div">
+            <Link
+                to={`/van/${id}`}
+                aria-label={`View details for ${van.name}, 
+                             priced at $${van.price} per day`}
+                className='van-card-link'
+            >
 
-                <div className="van-price-div">
+                <img
+                    src={imageUrl}
+                    alt={`Campervan called ${name}`}
+                    className="van-img"
+                />
+                <div className="van-details-div">
+                    <h2 className="van-name">
+                        {name}
+                    </h2>
 
-                    <span className="van-price">
-                        {`$${price} `}
+                    <div className="van-price-div">
+
+                        <span className="van-price">
+                            {`$${price} `}
+                        </span>
+                        <br />
+                        /day
+                    </div>
+                    <span className={`van-filter-tag ${type}-tag`}>
+                        {type}
                     </span>
-                    <br />
-                    /day
                 </div>
-                <span className={`van-filter-tag ${type}-tag`}>
-                    {type}
-                </span>
-            </div>
+            </Link>
 
         </div>
     )
