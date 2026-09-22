0f351713bf662145d6c616ad370015d19dc01d4b
Dav <david.sanchez.martinez@outlook.com>
2026-09-22 12:23:18 +0200

    refactor(routes): nested host & path updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import './App.css'
 import { Routes, Route } from 'react-router'
 import Home from './pages/Home'
 import About from './pages/About'
-import Vans from './pages/Vans/Vans'
-import VanDetail from './pages/Vans/VanDetail'
+import Vans from './pages/vans/Vans'
+import VanDetail from './pages/vans/VanDetail'
 import Layout from './components/Layout'
-import Dashboard from './pages/Host/Dashboard'
-import Income from './pages/Host/Income'
-import Reviews from './pages/Host/Reviews'
+import HostLayout from './components/HostLayout'
+import Dashboard from './pages/host/Dashboard'
+import Income from './pages/host/Income'
+import Reviews from './pages/host/Reviews'
 
 
 function App() {
@@ -15,48 +16,46 @@ function App() {
   return (
     <>
       <Routes>
-        <Route element={<Layout />}>
+        <Route
+          path='/'
+          element={<Layout />}
+        >
           <Route
-            path='/home'
+            index
             element={<Home />}
           />
-
           <Route
-            path='/about'
+            path='about'
             element={<About />}
           />
-
           <Route
-            path='/vans'
+            path='vans'
             element={<Vans />}
           />
-
           <Route
-            path='/van/:id'
+            path='van/:id'
             element={<VanDetail />}
           />
+
+          <Route
+            path='host'
+            element={<HostLayout />}
+          >
+            <Route
+              index
+              element={<Dashboard />}
+            />
+            <Route
+              path='income'
+              element={<Income />}
+            />
+            <Route
+              path='reviews'
+              element={<Reviews />}
+            />
+          </Route>
         </Route>
-
-        <Route
-          path='/host'
-          element={<Dashboard />}
-        />
-        <Route
-          path='/host/income'
-          element={<Income />}
-        />
-        <Route
-          path='/host/reviews'
-          element={<Reviews />}
-        />
-
       </Routes>
-
-      <footer>
-        <span className='footer-span'>
-          © 2026 #VANLIFE
-        </span>
-      </footer>
     </>
   )
 }
