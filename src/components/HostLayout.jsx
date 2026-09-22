6e0ed31c5a910a83cdc29f71f5fac656e3226269
Dav <david.sanchez.martinez@outlook.com>
2026-09-22 12:26:45 +0200

    feat: add host layout navigation

---
+++ src/components/HostLayout.jsx
@@ -0,0 +1,34 @@
+import { Link, Outlet } from 'react-router'
+
+const HostLayout = () => {
+
+    return (
+        <>
+            <nav>
+                <Link
+                    to='/host'
+                >
+                    Dashboard
+                </Link>
+                <Link
+                    to='/host/income'
+                >
+                    Income
+                </Link>
+                <Link
+                    to='/host/vans'
+                >
+                    Vans
+                </Link>
+                <Link
+                    to='/host/reviews'
+                >
+                    Reviews
+                </Link>
+            </nav>
+            <Outlet />
+        </>
+    )
+}
+
+export default HostLayout
