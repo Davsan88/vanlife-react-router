a476def7294b70eb0e163a79ea973f6042b3d767
Dav <david.sanchez.martinez@outlook.com>
2026-09-22 12:26:14 +0200

    feat: move Footer to a separate component and include in Layout

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+
+    return (
+        <footer>
+            <span className='footer-span'>
+                © 2026 #VANLIFE
+            </span>
+        </footer>
+    )
+}
+
+export default Footer
